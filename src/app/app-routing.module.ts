09ca7fb0fba7d4c5e6a1fed7c158e1b8485a532c
Omer-Cicek <omercicek160408@gmail.com>
2023-07-28 12:07:33 +0300

    pages created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EntertaintmentComponent } from './entertaintment/entertaintment.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'recipes',
+    component: HomeComponent,
+    // children: [
+    //   { path: 'entertainment', component: EntertaintmentComponent },
+    //   { path: 'new', component: RecipeEditComponent },
+    //   {
+    //     path: ':id',
+    //     component: RecipeDetailComponent,
+    //     resolve: [RecipesResolverService],
+    //   },
+    //   {
+    //     path: ':id/edit',
+    //     component: RecipeEditComponent,
+    //     resolve: [RecipesResolverService],
+    //   },
+    // ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BusinessComponent } from './business/business.component';
+import { EntertaintmentComponent } from './entertaintment/entertaintment.component';
+import { GeneralComponent } from './general/general.component';
+import { HealthComponent } from './health/health.component';
+import { ScienceComponent } from './science/science.component';
+import { SportsComponent } from './sports/sports.component';
+import { TechnologyComponent } from './technology/technology.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BusinessComponent,
+    EntertaintmentComponent,
+    GeneralComponent,
+    HealthComponent,
+    ScienceComponent,
+    SportsComponent,
+    TechnologyComponent
   ],
   imports: [
     BrowserModule,
