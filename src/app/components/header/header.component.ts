1d3ddc0edba476d28e1af48acd49e3808b5c826c
Omer-Cicek <omercicek160408@gmail.com>
2023-08-06 20:54:59 +0300

    toggle hamburger menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +7,28 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   activeTab: string = '';
+  width: string | number;
+  isHamburgerOpen: boolean = false;
+
+  dragging: boolean = false;
+
   setActiveTab(activeTab: string) {
     console.log(activeTab);
     this.activeTab = activeTab;
+  }
+
+  toggleHamburger() {
+    this.isHamburgerOpen = !this.isHamburgerOpen;
+    let open = this.isHamburgerOpen;
+    window.addEventListener('resize', function (event) {
+      //   console.log(event!.target!.outerWidth);
+      open = true;
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes);
+    console.log(window);
+    this.width = window.innerWidth;
   }
 }
