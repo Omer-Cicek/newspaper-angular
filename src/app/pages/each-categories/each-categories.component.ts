8d2d4217117392e5d150d319930ee85a230a00da
Omer-Cicek <omercicek160408@gmail.com>
2023-08-11 09:46:10 +0300

    All categories will be managed from this component

--- src/app/pages/each-categories/each-categories.component.ts
+++ src/app/pages/each-categories/each-categories.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { HeaderComponent } from 'src/app/components/header/header.component';
 
 @Component({
   selector: 'app-each-categories',
   templateUrl: './each-categories.component.html',
-  styleUrls: ['./each-categories.component.css']
+  styleUrls: ['./each-categories.component.css'],
 })
 export class EachCategoriesComponent {
-
+  @ViewChild(HeaderComponent) child;
 }
